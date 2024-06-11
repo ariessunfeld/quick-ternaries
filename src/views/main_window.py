a0e1eb154e0e860c341b5cd921b1f4a60021c20e
Ari <ariessunfeld@gmail.com>
2024-06-11 14:37:45 -0600

    removed debugging print statements

--- src/views/main_window.py
+++ src/views/main_window.py
@@ -94,7 +94,6 @@ class MainWindow(QMainWindow):
         self.dynamic_content_area.setCurrentWidget(self.ternary_start_setup_view)
     
     def switch_to_trace_view(self):
-        print('switch to trace view called in main window')
         self.dynamic_content_area.setCurrentWidget(self.ternary_trace_editor_view)
 
     def switch_plot_type(self, index):
