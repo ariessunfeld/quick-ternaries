81c228dde1f80db66296420997188d593e128016
reidmorris <100496249+reidmorris@users.noreply.github.com>
2024-06-28 13:33:43 -0700

    resolving merge mistake where method was duplicated

--- src/services/ternary/plot_maker.py
+++ src/services/ternary/plot_maker.py
@@ -177,23 +177,3 @@ class TernaryPlotMaker:
                 yanchor='top'
             )
         )
-
-    def save_plot(self, fig: Figure, filepath: str, dpi: float|None=None):
-        # Get the extension from the selected filter if the file_name has no extension
-        if filepath.endswith('.html'):
-            # Save interactive plot as HTML
-            with open(filepath, 'w', encoding='utf-8') as f:
-                f.write(fig.to_html())
-        else:
-            # Save static image with specified DPI
-            for trace in fig.data:
-                if 'marker' in trace and 'size' in trace.marker:
-                    original_size = trace.marker.size
-                    trace.marker.size = original_size / 1.8  # Halving the size
-            
-            pio.write_image(fig, filepath, scale=dpi / 72.0)
-            
-            for trace in fig.data:
-                if 'marker' in trace and 'size' in trace.marker:
-                    modified_size = trace.marker.size
-                    trace.marker.size = 1.8 * modified_size
