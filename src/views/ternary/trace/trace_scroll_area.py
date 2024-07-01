29dec13a2d54fd15eed5a1ecdb5860f97705a863
reidmorris <100496249+reidmorris@users.noreply.github.com>
2024-06-30 18:31:35 -0700

    merged reid's change: highlighted tab color and close button slightly darker (better for dark mode)

--- src/views/ternary/trace/trace_scroll_area.py
+++ src/views/ternary/trace/trace_scroll_area.py
@@ -62,7 +62,7 @@ class DraggableTab(QWidget):
         close_button.setStyleSheet("""
             QPushButton {
                 border: none;
-                background-color: lightgray;
+                background-color: #A9A9A9;
                 border-radius: 10px;
             }
             QPushButton:hover {
@@ -104,7 +104,7 @@ class DraggableTab(QWidget):
             self.show()
 
     def enterEvent(self, event):
-        self.label.setStyleSheet("background: lightgray;")
+        self.label.setStyleSheet("background: #A9A9A9;")
         super().enterEvent(event)
 
     def leaveEvent(self, event):
