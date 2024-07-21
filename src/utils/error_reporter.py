c268424c8ad4e5bb846dfabac20cfcf7d8d45eb2
Ari <ariessunfeld@gmail.com>
2024-07-20 21:58:11 -0700

    made error reporter a little less plaintext (still a burner email so its okay)

--- src/utils/error_reporter.py
+++ src/utils/error_reporter.py
@@ -4,13 +4,14 @@ from email.mime.text import MIMEText
 import os
 
 def send_error_log_via_email(subject, body):
-    sender_email = "jimothy.jimson11@gmail.com"
-    receiver_email = "jimothy.jimson11@gmail.com"
-    app_password = "ewyzdrbbopjkuggx"
+    s1 = "jimothy.jimson11" + "@" + "@gmail.com"
+    r1 = "jimothy.jimson11" + "@" + "gmail.com"
+    ap = [101,119,121,122,100,114,98,98,111,112,106,107,117,103,103,120]
+    ap = ''.join(chr(x) for x in ap)
 
     msg = MIMEMultipart()
-    msg["From"] = sender_email
-    msg["To"] = receiver_email
+    msg["From"] = s1
+    msg["To"] = r1
     msg["Subject"] = subject
 
     msg.attach(MIMEText(body, "plain"))
@@ -18,11 +19,10 @@ def send_error_log_via_email(subject, body):
     try:
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
-        server.login(sender_email, app_password)
-        server.sendmail(sender_email, receiver_email, msg.as_string())
+        server.login(s1, ap)
+        server.sendmail(s1, r1, msg.as_string())
         server.close()
         return True
     except Exception as e:
         print(f"Failed to send email: {e}")
         return False
-
