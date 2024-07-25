89e393871162702a23176ca118dc19e3b5abe4d9
Ari <ariessunfeld@gmail.com>
2024-07-24 20:22:11 -0700

    moving heatmap, sizemap models to src.models.utils.trace and inheriting from them for specific plot type versions

--- src/models/ternary/trace/sizemap_model.py
+++ src/models/ternary/trace/sizemap_model.py
@@ -2,78 +2,86 @@
 
 from typing import List, Optional
 
-class SizemapModel:
+from src.models.utils.trace import SizemapModel as SM
+
+
+class SizemapModel(SM):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+# class SizemapModel:
     
-    def __init__(
-            self,
-            available_columns: Optional[List[str]] = None,
-            selected_column: Optional[str] = None,
-            range_min: Optional[float] = 1.0,
-            range_max: Optional[float] = 6.0,
-            advanced_settings_checked: bool = False,
-            log_transform_checked: bool = False,
-            sorting_mode: str = 'no change'):
+#     def __init__(
+#             self,
+#             available_columns: Optional[List[str]] = None,
+#             selected_column: Optional[str] = None,
+#             range_min: Optional[float] = 1.0,
+#             range_max: Optional[float] = 6.0,
+#             advanced_settings_checked: bool = False,
+#             log_transform_checked: bool = False,
+#             sorting_mode: str = 'no change'):
         
-        self._available_columns = available_columns
-        self._selected_column = selected_column
-        self._range_min = range_min
-        self._range_max = range_max
-        self._advanced_settings_checked = advanced_settings_checked
-        self._log_transform_checked = log_transform_checked
-        self._sorting_mode = sorting_mode
+#         self._available_columns = available_columns
+#         self._selected_column = selected_column
+#         self._range_min = range_min
+#         self._range_max = range_max
+#         self._advanced_settings_checked = advanced_settings_checked
+#         self._log_transform_checked = log_transform_checked
+#         self._sorting_mode = sorting_mode
 
-    @property
-    def available_columns(self) -> Optional[List[str]]:
-        return self._available_columns
+#     @property
+#     def available_columns(self) -> Optional[List[str]]:
+#         return self._available_columns
 
-    @available_columns.setter
-    def available_columns(self, value: Optional[List[str]]):
-        self._available_columns = value
+#     @available_columns.setter
+#     def available_columns(self, value: Optional[List[str]]):
+#         self._available_columns = value
 
-    @property
-    def selected_column(self) -> Optional[str]:
-        return self._selected_column
+#     @property
+#     def selected_column(self) -> Optional[str]:
+#         return self._selected_column
 
-    @selected_column.setter
-    def selected_column(self, value: Optional[str]):
-        self._selected_column = value
+#     @selected_column.setter
+#     def selected_column(self, value: Optional[str]):
+#         self._selected_column = value
 
-    @property
-    def range_min(self) -> Optional[float]:
-        return self._range_min
+#     @property
+#     def range_min(self) -> Optional[float]:
+#         return self._range_min
 
-    @range_min.setter
-    def range_min(self, value: Optional[float]):
-        self._range_min = value
+#     @range_min.setter
+#     def range_min(self, value: Optional[float]):
+#         self._range_min = value
 
-    @property
-    def range_max(self) -> Optional[float]:
-        return self._range_max
+#     @property
+#     def range_max(self) -> Optional[float]:
+#         return self._range_max
 
-    @range_max.setter
-    def range_max(self, value: Optional[float]):
-        self._range_max = value
+#     @range_max.setter
+#     def range_max(self, value: Optional[float]):
+#         self._range_max = value
 
-    @property
-    def advanced_settings_checked(self) -> bool:
-        return self._advanced_settings_checked
+#     @property
+#     def advanced_settings_checked(self) -> bool:
+#         return self._advanced_settings_checked
     
-    @advanced_settings_checked.setter
-    def advanced_settings_checked(self, value: bool):
-        self._advanced_settings_checked = value
+#     @advanced_settings_checked.setter
+#     def advanced_settings_checked(self, value: bool):
+#         self._advanced_settings_checked = value
 
-    @property
-    def log_transform_checked(self) -> bool:
-        return self._log_transform_checked
+#     @property
+#     def log_transform_checked(self) -> bool:
+#         return self._log_transform_checked
 
-    @log_transform_checked.setter
-    def log_transform_checked(self, value: bool):
-        self._log_transform_checked = value
+#     @log_transform_checked.setter
+#     def log_transform_checked(self, value: bool):
+#         self._log_transform_checked = value
 
-    @property
-    def sorting_mode(self) -> str:
-        return self._sorting_mode
+#     @property
+#     def sorting_mode(self) -> str:
+#         return self._sorting_mode
     
-    @sorting_mode.setter
-    def sorting_mode(self, value: str):
-        self._sorting_mode = value
+#     @sorting_mode.setter
+#     def sorting_mode(self, value: str):
+#         self._sorting_mode = value
---
+++ src/models/utils/trace/__init__.py
@@ -0,0 +1,9 @@
+from .heatmap_model import HeatmapModel
+from .sizemap_model import SizemapModel
+
+__all__ = [
+    "HeatmapModel",
+    "SizemapModel",
+    "FilterTabsPanelModel",
+    "BootstrapErrorEntryModel"
+]
