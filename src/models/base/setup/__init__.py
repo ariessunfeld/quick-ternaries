890487b0e27c7ef3204c807fb02b02bd20656661
Ari <ariessunfeld@gmail.com>
2024-07-25 23:12:34 -0700

    updated init

--- src/models/base/setup/__init__.py
+++ src/models/base/setup/__init__.py
@@ -1,6 +1,7 @@
 from .base_scaling_model import BaseAxisScalingModel
+from .base_hover_selection_model import BaseHoverDataSelectionModel
 
 __all__ = [
     "BaseAxisScalingModel",
-    
+    "BaseHoverDataSelectionModel"
 ]
