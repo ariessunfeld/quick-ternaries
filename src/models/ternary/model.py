f9920608401fc47435b409970258c2702242dbbe
Ari <ariessunfeld@gmail.com>
2024-07-25 17:45:28 -0700

    fixing circular imports

--- src/models/ternary/model.py
+++ src/models/ternary/model.py
@@ -1,9 +1,12 @@
+from typing import TYPE_CHECKING
+
 from src.models.ternary.setup import TernaryStartSetupModel
 from src.models.ternary.trace import MolarConversionModel
-from src.models.utils import TabsPanelModel
-from src.models.ternary.plot import TernaryRenderPlotModel
 
-from src.models.utils import DataLibrary
+
+if TYPE_CHECKING:
+    from src.models.utils import TabsPanelModel
+    from src.models.utils import DataLibrary
 
 class TernaryModel:
     """Container class for ternary plotting"""
@@ -15,8 +18,8 @@ class TernaryModel:
         self.data_library = None
         self.tab_model = None
 
-    def set_data_library(self, data_library: DataLibrary):
+    def set_data_library(self, data_library: 'DataLibrary'):
         self.data_library = data_library
 
-    def set_tab_model(self, tab_model: TabsPanelModel):
+    def set_tab_model(self, tab_model: 'TabsPanelModel'):
         self.tab_model = tab_model
