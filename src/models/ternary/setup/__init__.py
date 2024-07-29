258106c822281788c3a0806684924d52ddcd4c16
Ari <ariessunfeld@gmail.com>
2024-07-29 11:14:41 -0600

    removing old import

--- src/models/ternary/setup/__init__.py
+++ src/models/ternary/setup/__init__.py
@@ -1,6 +1,5 @@
 from .advanced_settings_model import AdvancedSettingsModel
 from .apex_scaling_model import TernaryApexScalingModel
-from .custom_apex_selection_model import CustomApexSelectionModel
 from .custom_apex_selection_model import AxisSelectionModel
 from .custom_hover_data_selection_model import CustomHoverDataSelectionModel
 from .model import TernaryType, TernaryStartSetupModel
@@ -8,7 +7,6 @@ from .model import TernaryType, TernaryStartSetupModel
 __all__ = [
     "AdvancedSettingsModel",
     "TernaryApexScalingModel",
-    "CustomApexSelectionModel",
     "CustomHoverDataSelectionModel",
     "AxisSelectionModel",
     "TernaryType", "TernaryStartSetupModel"
