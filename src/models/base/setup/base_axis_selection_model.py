db974fa2bc4899f31c3443c51f4f751ffecbd426
Ari <ariessunfeld@gmail.com>
2024-07-29 11:45:20 -0600

    moved methods from ternary axis selection model to base selection model

--- src/models/base/setup/base_axis_selection_model.py
+++ src/models/base/setup/base_axis_selection_model.py
@@ -4,6 +4,9 @@ from typing import List, Optional
 
 
 class BaseAxisSelectionModel:
+
+    AXES = []
+    
     def __init__(self, options: Optional[List[str]] = None, selected: str = ''):
         self._options = options or []
         self._selected = selected
@@ -23,6 +26,35 @@ class BaseAxisSelectionModel:
     @selected.setter
     def selected(self, value: str):
         self._selected = value
+
+    def add_option(self, option: str):
+        if option not in self._options:
+            self._options.append(option)
+    
+    def rem_option(self, option: str):
+        if option in self._options:
+            self._options.remove(option)
+
+    def add_to_axis(self, option: str, axis: str):
+        """Adds `option` to `axis` and removes `option` from `options`"""
+        if axis in self.AXES and option in self._options:
+            current = getattr(self, f'_{axis}')
+            if option not in current:
+                setattr(self, f'_{axis}', current + [option])
+                self.rem_option(option)
+        else:
+            raise ValueError(f'`{axis}` not a valid axis ({self.AXES}) or `{option}` not available.')
+
+    def rem_from_axis(self, option: str, axis: str):
+        """Removes `option` from `axis` and adds `option` back to `options`"""
+        if axis in self.AXES:
+            current = getattr(self, f'_{axis}')
+            if option in current:
+                current.remove(option)
+                setattr(self, f'_{axis}', current)
+                self.add_option(option)
+        else:
+            raise ValueError(f'`{axis}` not a valid axis ({self.AXES})')
 
     def to_json(self) -> dict:
         """Convert the model to a JSON-serializable dictionary."""
--- src/models/ternary/setup/custom_apex_selection_model.py
+++ src/models/ternary/setup/custom_apex_selection_model.py
@@ -44,32 +44,3 @@ class TernaryAxisSelectionModel(BaseAxisSelectionModel):
     @right.setter
     def right(self, value: Optional[List[str]]):
         self._right = value.copy()
-
-    def add_option(self, option: str):
-        if option not in self._options:
-            self._options.append(option)
-    
-    def rem_option(self, option: str):
-        if option in self._options:
-            self._options.remove(option)
-
-    def add_to_axis(self, option: str, axis: str):
-        """Adds `option` to `axis` and removes `option` from `options`"""
-        if axis in self.AXES and option in self._options:
-            current = getattr(self, f'_{axis}')
-            if option not in current:
-                setattr(self, f'_{axis}', current + [option])
-                self.rem_option(option)
-        else:
-            raise ValueError(f'`{axis}` not a valid axis ({self.AXES}) or `{option}` not available.')
-
-    def rem_from_axis(self, option: str, axis: str):
-        """Removes `option` from `axis` and adds `option` back to `options`"""
-        if axis in self.AXES:
-            current = getattr(self, f'_{axis}')
-            if option in current:
-                current.remove(option)
-                setattr(self, f'_{axis}', current)
-                self.add_option(option)
-        else:
-            raise ValueError(f'`{axis}` not a valid axis ({self.AXES})')
